ad70f12a2f47ec8198e3eb3c9e9251c5d53a0780
hammi1972 <hammi11121972@gmail.com>
2026-03-08 17:46:13 +0100

    Calculate GPA based on credits and grade

--- Lab2_GPA/gpa.php
+++ Lab2_GPA/gpa.php
@@ -1,1 +1,16 @@
+<?php
+$credits = $_POST["credits"];
+$grade = $_POST["grade"];
 
+switch($grade) {
+    case "A": $points = 4; break;
+    case "B": $points = 3; break;
+    case "C": $points = 2; break;
+    case "D": $points = 1; break;
+    default: $points = 0;
+}
+
+$gpa = $points * $credits;
+
+echo "النقاط: " . $gpa;
+?>
