28f4b56f7f0417d98d99137ce7392dc7fa3317ce
hammi1972 <hammi11121972@gmail.com>
2026-03-08 17:59:02 +0100

    Replace calculateGPA with validateForm function
    Refactored GPA calculation into a form validation function.

--- Lab2_GPA/script.js
+++ Lab2_GPA/script.js
@@ -1,13 +1,8 @@
-function calculateGPA(){
+function validateForm() {
+    let credits = document.getElementById("credits").value;
 
-let g1 = parseFloat(document.getElementById("grade1").value) || 0
-let g2 = parseFloat(document.getElementById("grade2").value) || 0
-let g3 = parseFloat(document.getElementById("grade3").value) || 0
-
-let total = g1 + g2 + g3
-let gpa = total / 3
-
-document.getElementById("result").innerText =
-"Your GPA is: " + gpa.toFixed(2)
-
+    if (credits <= 0) {
+        alert("عدد الساعات غير صحيح");
+        return false;
+    }
 }
