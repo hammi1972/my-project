834a3a41a532d8a34eeb62dfc3ac958fc40bf612
hammi1972 <hammi11121972@gmail.com>
2026-03-08 17:43:31 +0100

    Add GPA calculation function

--- Lab2_GPA/script.js
+++ Lab2_GPA/script.js
@@ -1,1 +1,13 @@
+function calculateGPA(){
 
+let g1 = parseFloat(document.getElementById("grade1").value) || 0
+let g2 = parseFloat(document.getElementById("grade2").value) || 0
+let g3 = parseFloat(document.getElementById("grade3").value) || 0
+
+let total = g1 + g2 + g3
+let gpa = total / 3
+
+document.getElementById("result").innerText =
+"Your GPA is: " + gpa.toFixed(2)
+
+}
